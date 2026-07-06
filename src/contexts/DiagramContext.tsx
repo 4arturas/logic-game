3d25ec17df15eb3a2a55f62aa352dd8cc3a5c408
Your Name <you@example.com>
2026-07-06 16:35:58 +0300

    Remove everything except docs folder

--- src/contexts/DiagramContext.tsx
+++
@@ -1,9 +0,0 @@
-export interface DiagramState {
-  [key: string]: 'red' | 'grey' | null
-}
-
-export interface Terms {
-  x: string
-  y: string
-  m: string
-}
--- src/routes/practice.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, redirect } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/practice')({
-  beforeLoad: async () => {
-    throw redirect({
-      to: '/',
-    })
-  },
-})
